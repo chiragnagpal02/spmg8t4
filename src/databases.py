15eb76f282beba8d1c06ad515aa49252acd8a1b7
timus tay <111484493+TimusAA@users.noreply.github.com>
2023-09-19 22:04:19 +0800

    python and flask changes

---
+++ src/databases.py
@@ -0,0 +1,6 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+# Creating all the individual Tables inside the db Database -> Each MS to have its own Schema 
