1bcd042b9f13b843bd333bcf91c3ba8c9c39795c
T-Leng <tleng.eng.2021@scis.smu.edu.sg>
2023-09-24 22:47:28 +0800

    Hrview own page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import StaffPage from './components/Staff/StaffPage'
 import HRPage from './components/HR/HRPage'
 import ManagerPage from './components/Manager/ManagerPage'
+import HRview from './components/HR/ViewJob'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/staff" element={<StaffPage />} />
         <Route path="/hr" element={<HRPage />} />
+        <Route path="/hrview" element={<HRview />} />
         <Route path="/manager" element={<ManagerPage />} />
       </Routes>
     </Router>
