a132d120f61d08d9873f4bdf5567700004e11858
Chirag Nagpal <85030123+chiragnagpal02@users.noreply.github.com>
2023-10-06 12:59:03 +0800

    adding createjob and homepage files

--- src/components/HR/CreateJob.jsx
+++ src/components/HR/CreateJob.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useState } from 'react'
 import Modal from './Modal';
 
+
+
 const CreateJob = () => {
     const [skills, setSkills] = useState([]);
     const [isModalVisible, setIsModalVisible] = useState(false);
--- src/trial.py
+++ src/trial.py
@@ -3,6 +3,11 @@ from flask import Blueprint, request, jsonify, render_template
 tryme = Blueprint('tryme', __name__, url_prefix='/tryme')
 
 
+
 @tryme.route('/')
 def index():
+    """
+    A decorator that defines the route for the index page.
+    Returns a string that says "Hello, tryme!".
+    """
     return "Hello, tryme!"
