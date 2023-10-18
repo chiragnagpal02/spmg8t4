3a7143e70870e9e31470d5e3587bddde7bbb15de
Anandini134 <83274295+Anandini134@users.noreply.github.com>
2023-10-18 13:13:07 +0800

    c

--- src/App.jsx
+++ src/App.jsx
@@ -13,26 +13,25 @@ import ApplyJobPage from './components/Staff/ApplyJobPage';
 import HRmatch from './components/HR/Viewmatch';
 import StaffSkillsComponent from './StaffSkillsComponent';
 
-  render( {
-    return (
-      <Router>
-        <Routes>
-          
-          <Route path="/staff/viewalljobs" element={<AllJobs />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/staff" element={<StaffPage />} />
-          <Route path="/staff/apply/:listing_id" element={<ApplyJobPage />} />
-          <Route path="/hr" element={<HRPage />} />
-          <Route path="/hrview" element={<HRview />} />
-          <Route path="/hrmatch" element={<HRmatch />} />
-          <Route path="/manager" element={<ManagerPage />} />
-          <Route path="/staff/staffskills" element={<StaffSkillsComponent />} />
-          
-        </Routes>
-      </Router>
-    );
-    }
-  
+render() {
+  return (
+    <Router>
+      <Routes>
+        {/* Other Routes */}
+        <Route path="/staff/viewalljobs" element={<AllJobs />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/staff" element={<StaffPage />} />
+        <Route path="/staff/apply/:listing_id" element={<ApplyJobPage />} />
+        <Route path="/hr" element={<HRPage />} />
+        <Route path="/hrview" element={<HRview />} />
+        <Route path="/hrmatch/:role_listing_id" element={<HRmatch />} />
+        <Route path="/manager" element={<ManagerPage />} />
+        <Route path="/staff/staffskills" element={<StaffSkillsComponent />} />
+      </Routes>
+    </Router>
+  );
+}
+
 
 
 export default App;
