3e149e8ea53301ecde66be132e3a504235db299e
T-Leng <tleng.eng.2021@scis.smu.edu.sg>
2023-10-24 17:22:44 +0800

    HRview added filters

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 # app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("dbURL")
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://spm@localhost:8889/SBRP_G8T4"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root@localhost:3306/SBRP_G8T4"
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"pool_recycle": 299}
