e13fc311f303f15fb3b4100db9aedf01aac4799b
boopyschloopy <atrayeed.2021@scis.smu.edu.sg>
2023-11-01 22:37:20 +0800

    Updated warning and success messages

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("dbURL")
+#app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("dbURL")
+app.config["SQLALCHEMY_DATABASE_URI"]="mysql+mysqlconnector://spm@localhost:8889/SBRP_G8T4"
+
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"pool_recycle": 299}
